c85a00c91e52ffd32038787a48e6b6851877ffe4
Thiago Magano <thiagomagano1993@outlook.com>
2020-06-18 18:13:42 -0300

    Frontend finalizado

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./styles.css";
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
 import { FiLogIn } from "react-icons/fi";
+import api from "../../services/api";
 
 function Logon() {
+  const [id, setId] = useState("");
+
+  const history = useHistory();
+
+  async function HandleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+
+      history.push("/profile");
+    } catch (err) {
+      alert("Falha no login, tente novamente");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={HandleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua Id" />
+          <input
+            placeholder="Sua Id"
+            value={id}
+            onChange={(event) => setId(event.target.value)}
+          />
           <button type="submit" className="button">
             Entrar
           </button>
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import logoImg from "../../assets/logo.svg";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  const history = useHistory();
+
+  async function handlerRegister(event) {
+    event.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+    try {
+      const response = await api.post("ongs", data);
+      alert(`Seu id de acesso: ${response.data.id}`);
+      history.push("/");
+    } catch (err) {
+      alert("Cadastro com erro, tente novamente");
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -22,13 +50,35 @@ export default function Register() {
             Voltar para login
           </Link>
         </section>
-        <form>
-          <input placeholder="Nome da ONG" />
-          <input type="email" placeholder="E-Mail" />
-          <input type="tel" placeholder="WhatsApp" />
+        <form onSubmit={handlerRegister}>
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-Mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            placeholder="WhatsApp"
+            value={whatsapp}
+            onChange={(event) => setWhatsapp(event.target.value)}
+          />
           <div className="input-group">
-            <input placeholder="Cidade" />
-            <input placeholder="UF" style={{ width: 80 }} />
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+            />
+            <input
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={(event) => setUf(event.target.value)}
+            />
           </div>
           <button className="button">Cadastrar</button>
         </form>
